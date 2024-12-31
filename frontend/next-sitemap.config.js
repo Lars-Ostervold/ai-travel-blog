025fb8ff60c7b9b87426ae38e4e226ac20e27336
Lars-Ostervold <ostervold.berent@gmail.com>
2024-12-31 10:18:37 -0600

    Automatic sitemap generation

---
+++ frontend/next-sitemap.config.js
@@ -0,0 +1,7 @@
+const SITE_URL = 'https://www.chasingmemories.com';
+
+module.exports = {
+  siteUrl: SITE_URL,
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -2,3 +2,4 @@ export const EXAMPLE_PATH = "blog-starter";
 export const CMS_NAME = "Markdown";
 export const HOME_OG_IMAGE_URL = "https://nxvznoqipejdootcntuo.supabase.co/storage/v1/object/public/character-reference/chasing_memories_graph_image.png?t=2024-12-21T15%3A13%3A47.511Z";
 export const BLOG_TITLE = "Chasing Memories: A Family Travel Blog";
+export const SITE_URL = "https://chasingmemories.com";
