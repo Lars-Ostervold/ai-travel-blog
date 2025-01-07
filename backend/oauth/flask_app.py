d55f1ffb3472585dc68ab19051b7fdaf73e571fc
Lars-Ostervold <ostervold.berent@gmail.com>
2025-01-07 09:18:16 -0600

    Preview available pins in Flask app

--- backend/oauth/flask_app.py
+++ backend/oauth/flask_app.py
@@ -3,13 +3,13 @@ import requests
 import os
 import base64
 from dotenv import load_dotenv
+load_dotenv()
 
 from supabase import create_client, Client
 supabase_url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(supabase_url, key)
 
-load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'  # Replace with your secret key
@@ -19,12 +19,31 @@ CLIENT_ID = os.getenv('PINTEREST_APP_ID')
 CLIENT_SECRET = os.getenv('PINTEREST_APP_SECRET')
 REDIRECT_URI = os.getenv('REDIRECT_URI')
 
+class Pin:
+    def __init__(self, title, description, media_url, link):
+        self.title = title
+        self.description = description
+        self.media_url = media_url
+        self.link = link
+    def to_dict(self):
+        return {
+            "title": self.title,
+            "description": self.description,
+            "media_url": self.media_url,
+            "link": self.link
+        }
+
 def fetch_most_recent_blog_post():
     response = supabase.table('blog_posts').select('*').order('created_at', desc=True).limit(1).execute()
     if response.data:
         return response.data[0]
     return None
 
+'''
+Home route of the application will render the auth.html template if no code exists.
+If a code exists, the application will make a POST request to the Pinterest API to exchange the code for an access token.
+If the request is successful, the access token will be saved in the session and the success.html template will be rendered.
+'''
 @app.route('/')
 def pinterest_callback():
     code = request.args.get('code')
@@ -71,13 +90,31 @@ def auth_pinterest():
     )
     return redirect(pinterest_url)
 
-@app.route('/generate_pin')
-def generate_pin():
-    blog_post = fetch_most_recent_blog_post()
-    if blog_post:
-        session['blog_post'] = blog_post
-        return render_template('preview.html', blog_post=blog_post)
-    return "No blog post found"
+'''
+The /available_pins will display all possible pins, sorted from newest to oldest.
+Will include the photo, title, and description of the blog post.
+'''
+@app.route('/available_pins')
+def available_pins():
+    #We have two tables, blog_posts and blog_images.
+    #The goal is to get every blog_image, then get the title and excerpt from the blog_post table where blog_post_id in blog_images matches id in blog_post.
+    #Each one of those objects should be a new pin.
+    response = supabase.table('blog_images').select('*').execute()
+    pins = []
+    for blog_image in response.data:
+        blog_post_id = blog_image.get('blog_post_id')
+        blog_post_response = supabase.table('blog_posts').select('*').eq('id', blog_post_id).execute()
+        blog_post = blog_post_response.data[0]
+        title = blog_post.get('title')
+        description = blog_post.get('excerpt')
+        media_url = blog_image.get('image_url')
+        link = f"https://chasingmemories.blog/posts/{blog_post_id}_blog"
+        pin = Pin(title, description, media_url, link)
+        print(pin)
+        pins.append(pin.to_dict())
+    print("---------PINS---------")
+    print(pins)
+    return render_template('available_pins.html', pins=pins)
 
 @app.route('/confirm_pin', methods=['POST'])
 def confirm_pin():
