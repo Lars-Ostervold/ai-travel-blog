f3bbf02aefe820ce444bea85d5a8f857f165ef7f
Lars-Ostervold <ostervold.berent@gmail.com>
2025-01-21 16:29:01 -0600

    docker for pinterest simplified

--- backend/pinterest/lambda_function.py
+++ backend/pinterest/lambda_function.py
@@ -1,7 +1,7 @@
 # import post_all_pins
 # import post_random_pin_single_board
-# import post_random_pin_multiple_boards
-import post_recent_pin
+import post_random_pin_multiple_boards
+# import post_recent_pin
 
 def handler(event, context):
     # Call the function from generate_blog_post.py
@@ -9,8 +9,8 @@ def handler(event, context):
     #Options in each image
     # post_all_pins.main()
     # post_random_pin_single_board.main()
-    # post_random_pin_multiple_boards.main()
-    post_recent_pin.main()
+    post_random_pin_multiple_boards.main()
+    # post_recent_pin.main()
 
     
     return {
