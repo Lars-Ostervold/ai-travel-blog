f23099ffdd4f2a3d76883178706c101c85842284
Lars-Ostervold <ostervold.berent@gmail.com>
2025-02-17 08:38:16 -0600

    adjust image display for 2:3 aspect ratio

--- frontend/src/app/_components/cover-image.tsx
+++ frontend/src/app/_components/cover-image.tsx
@@ -13,15 +13,15 @@ const CoverImage = ({ title, src, slug }: Props) => {
     <Image
       src={src}
       alt={`Cover Image for ${title}`}
-      className={cn("shadow-sm w-full", {
+      className={cn("shadow-sm m-auto", {
         "hover:shadow-lg transition-shadow duration-200": slug,
       })}
-      width={1300}
-      height={630}
+      width={867} // Adjusted width for 2:3 aspect ratio
+      height={1300} // Adjusted height for 2:3 aspect ratio
     />
   );
   return (
-    <div className="sm:mx-0">
+    <div className="sm:mx-0 max-w-full">
       {slug ? (
         <Link href={`/posts/${slug}`} aria-label={title}>
           {image}
